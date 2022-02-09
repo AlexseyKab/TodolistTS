b2ed0e4adb10b097d38e60b0c04312973951d0c5
AlexseyKab <help_me101@mail.ru>
2022-02-09 12:47:33 +0300

    Updates

---
+++ precache-manifest.5d2589f5d5e0afad29133ef96280fc59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac1606b8b46df6924fc19bed825f73c",
+    "url": "/TodolistTS/index.html"
+  },
+  {
+    "revision": "7dacdae3e9e922852a5b",
+    "url": "/TodolistTS/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "00393688e30f55e74291",
+    "url": "/TodolistTS/static/js/2.757bc891.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/TodolistTS/static/js/2.757bc891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dacdae3e9e922852a5b",
+    "url": "/TodolistTS/static/js/main.2dff7c42.chunk.js"
+  },
+  {
+    "revision": "d4a7554a56ee5559dd78",
+    "url": "/TodolistTS/static/js/runtime-main.ebe2e2d4.js"
+  }
+]);
